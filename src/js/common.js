a8c54105edd2bf903e4b02188215505298df98ec
alex-lenk <artlenk.ru@gmail.com>
2018-07-03 15:59:34 +0300

    Created language-bar

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,22 @@
 $(document).ready(function () {
+
+    /* Begin language-bar */
+
+    var langArea = $(".language-bar");
+    var $langItem = $('.lang-item');
+    var langOpen = 'lang-open';
+
+    var maxWidth = Array.prototype.reduce.call($langItem, function (result, item) {
+        return Math.max(result, item.offsetWidth);
+    }, 0);
+
+    langArea.width(maxWidth);
+
+    langArea.click(
+        function () {
+            $(this).toggleClass(langOpen);
+        }
+    );
+    /* END language-bar */
+
 });

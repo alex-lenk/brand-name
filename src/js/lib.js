c6d7c9af9adc6e2b35b014fcbd1f15531332ce74
alex-lenk <artlenk.ru@gmail.com>
2018-07-04 17:03:00 +0300

    Created product-info and see-also blocks

--- src/js/lib.js
+++ src/js/lib.js
@@ -4,9 +4,11 @@
  * 1. smoothscroll.js
  * 2. jquery.sliderPro.min.js
  * 3. jquery.formstyler.min.js
+ * 4. bootstrap.min.js
  *
  */
 
 //= lib/smoothscroll.js
 //= ../../bower_components/slider-pro/dist/js/jquery.sliderPro.min.js
 //= lib/jquery.formstyler.min.js
+//= lib/bootstrap.min.js
